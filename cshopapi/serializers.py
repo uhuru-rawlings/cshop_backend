f562acb9d65c866f64a91b1b663ea3100ca9f158
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-16 22:06:48 +0300

    added serializer fields

--- cshopapi/serializers.py
+++ cshopapi/serializers.py
@@ -11,3 +11,19 @@ class CategorySerializer(serializers.Modelserializer):
     class Meta:
         model = Category
         fields = ["id","itemcategory"]
+
+class ClothesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clothes
+        fields = ["id","category","item_name","item_color","item_description","item_quantity","item_price","for_gender","date_added"]
+
+class CheckoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Checkout
+        fields = ["id","user","pickup_date","deliveryCity","delivery_address","receiver_id","receiver_id","added_date"]
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Cart
+        fields = ["id","user","items","quantity","price","date_added"]
