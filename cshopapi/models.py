0dbfa03c486938964d9e204e6db3b90b7022d9d0
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-16 16:34:22 +0300

    added required imports"

--- cshopapi/models.py
+++ cshopapi/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.hashers import make_password,check_password
+import jwt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
 # Create your models here.
