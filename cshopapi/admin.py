1ac08bc39f29fe002da6aad9bfe9f699e0db56b0
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-16 17:31:03 +0300

    registered models

--- cshopapi/admin.py
+++ cshopapi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from cshopapi.models import  Registration,Category,Clothes,Checkout,Cart
 # Register your models here.
+admin.site.register(Registration)
+admin.site.register(Category)
+admin.site.register(Clothes)
+admin.site.register(Checkout)
+admin.site.register(Cart)
