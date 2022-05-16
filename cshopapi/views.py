1c7e17e709003806c2244fccbd17ac25939f3f15
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-16 22:14:41 +0300

    added register user endpoint

--- cshopapi/views.py
+++ cshopapi/views.py
@@ -4,4 +4,19 @@ import jwt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 from cshopapi.models import  Registration,Category,Clothes,Checkout,Cart
+from cshopapi.serializers import RegistrationSerializers,CategorySerializer,ClothesSerializer,CheckoutSerializer,CartSerializer
 # Create your views here.
+
+@api_view(['POST'])
+def register_user(request):
+    details = request.data
+    useremail = details['useremail']
+    password = details['password']
+
+    checkuser = Registration.objects.filter(useremail = useremail)
+    if checkuser.exists():
+        return Response({"error":"sorry this user already exist."})
+    else:
+        new_user = Registration(useremail = useremail,password = make_password(password))
+        new_user.save()
+        return Response({"success":"Registration successful."})
