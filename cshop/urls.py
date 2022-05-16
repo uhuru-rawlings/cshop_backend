88824c973223367a18a1e21ef38207818a675a68
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-16 16:44:39 +0300

    added swaager path url

--- cshop/urls.py
+++ cshop/urls.py
@@ -1,12 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-
-from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Pastebin API')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', schema_view)
+    path('swagger/', schema_view)
 ]
