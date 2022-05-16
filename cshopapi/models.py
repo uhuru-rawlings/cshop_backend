b5fd28c05d527b64b06415cd97b44f0963870d0d
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-16 16:49:02 +0300

    added registration models

--- cshopapi/models.py
+++ cshopapi/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Registration(models.Model):
+    useremail = models.EmailField(max_length=300)
+    password = models.CharField(max_length=366)
+    date_added = models.DateTimeField(auto_now=True)
