021a1393ee8a29b2c821d53be80f6b51bf9f9751
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-16 17:31:46 +0300

    imported models to the view

--- cshopapi/views.py
+++ cshopapi/views.py
@@ -3,4 +3,5 @@ from django.contrib.auth.hashers import make_password,check_password
 import jwt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from cshopapi.models import  Registration,Category,Clothes,Checkout,Cart
 # Create your views here.
