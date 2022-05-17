066eb753e0a8561a61c96d3bfb3ddfc8180b40e4
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-17 09:05:52 +0300

    added requirements.txt

--- cshopapi/serializers.py
+++ cshopapi/serializers.py
@@ -1,13 +1,13 @@
 from pyexpat import model
-from rest_framework.serializers import serializers
+from rest_framework import serializers
 from cshopapi.models import  Registration,Category,Clothes,Checkout,Cart
 
-class RegistrationSerializers(serializers.ModelsSerializer):
+class RegistrationSerializers(serializers.ModelSerializer):
      class Meta:
          model = Registration
          fields = ["id","useremail"]
 
-class CategorySerializer(serializers.Modelserializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id","itemcategory"]
