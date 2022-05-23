2d366291ff998a2f052a30593d545cfdef9003fa
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-23 08:01:34 +0300

    final commits

--- cshop/settings.py
+++ cshop/settings.py
@@ -173,12 +173,14 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:4200"
+    "http://127.0.0.1:4200",
+    "https://rawlycshop.netlify.app"
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
      "http://127.0.0.1:4200",
+     "https://rawlycshop.netlify.app"
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
